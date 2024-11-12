65fa07fdfb9b5999a6760823dcc2f73f38d74aa2
D4V1D-101 <115972590+D4V1D-101@users.noreply.github.com>
2024-11-12 14:01:07 +0100

    buta voltam nem 2000000 hanem 20 :) :)

--- Varosok/Program.cs
+++ Varosok/Program.cs
@@ -9,8 +9,8 @@ while (!sr.EndOfStream)
     adatoks.Add(new Adatok(sr.ReadLine()));
 }
 
-var f1 = adatoks.Where(v=>v.OrszagNeve == "Kína").Sum(v=>v.Nepesseg);
-Console.WriteLine($"ennyi ember él a Kínai nagyvárosokban: {f1}");
+var f1 = adatoks.Where(v => v.OrszagNeve == "Kína").Sum(v => v.Nepesseg);
+Console.WriteLine($"ennyi ember él a Kínai nagyvárosokban: {f1} M");
 
 
 var f2 = adatoks.Where(v => v.OrszagNeve == "India").Average(v => v.Nepesseg);
@@ -19,23 +19,19 @@ Console.WriteLine($"ennyi az indiai nagyvárosok átlaglélekszáma: {f2}");
 var f3 = adatoks.MaxBy(v => v.Nepesseg);
 Console.WriteLine($"ez a nagyvárosa a legnépesebb: {f3.VarosNeve}");
 
-var f4 = adatoks.Where(v => v.Nepesseg > 20000000).OrderByDescending(v => v.Nepesseg).ToList();
-
-
+var f4 = adatoks.Where(v => v.Nepesseg > 20).OrderByDescending(v => v.Nepesseg).ToList();
+Console.WriteLine("20 millió fő feletti nagyvárosok népesség szerint csökkenő sorrendben:");
 
 foreach (var item in f4)
 {
-    Console.WriteLine($"{item.VarosNeve} - {item.Nepesseg}");
+    Console.WriteLine($"{item.VarosNeve} - {item.Nepesseg} M");
 }
 
-
 var f5 = adatoks.GroupBy(v => v.OrszagNeve).Where(v => v.Count() > 1).Count();
-Console.WriteLine($"Azon oszágok száma hol több nagyváros is szerepel: {f5}");
-
+Console.WriteLine($"Azon Országok száma ahol több nagyváros is szerepel: {f5}");
 
 var f6 = adatoks.GroupBy(v => v.VarosNeve[0]).OrderByDescending(g => g.Count()).First();
-
-Console.WriteLine($"A legtöbb nagyváros neve {f6.Key}-vel kezdődik, összesen {f6.Count()} város.");
+Console.WriteLine($"A legtöbb nagysváros neve {f6.Key}-vel kezdődik, ami összesen {f6.Count()} város.");
 
 
 
