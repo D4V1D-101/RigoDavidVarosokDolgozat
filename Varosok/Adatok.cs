2e230110a8e96d2af74a0d8ab4b61f011de00f01
D4V1D-101 <115972590+D4V1D-101@users.noreply.github.com>
2024-11-12 13:51:19 +0100

    Add project files.

---
+++ Varosok/Adatok.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varosok
+{
+    internal class Adatok
+    {
+        public Adatok(string r)
+        {
+            var v = r.Split(';');
+            VarosNeve = v[0];
+            OrszagNeve = v[1];
+            Nepesseg = double.Parse(v[2]);
+        }
+
+        public string VarosNeve { get; set; }
+        public string OrszagNeve { get; set; }
+        public double Nepesseg { get; set; }
+
+        public override string ToString()
+        {
+            return $"{VarosNeve} {OrszagNeve} {Nepesseg}";
+        }
+
+    }
+    
+}
---
+++ Varosok/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Varosok;
+
+List<Adatok> adatoks = new List<Adatok>();
+using StreamReader sr = new($"../../../src/varosok.csv", encoding: System.Text.Encoding.UTF8);
+string w = sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    adatoks.Add(new Adatok(sr.ReadLine()));
+}
+
+var f1 = adatoks.Where(v=>v.OrszagNeve == "Kína").Sum(v=>v.Nepesseg);
+Console.WriteLine($"ennyi ember él a Kínai nagyvárosokban: {f1}");
+
+
+var f2 = adatoks.Where(v => v.OrszagNeve == "India").Average(v => v.Nepesseg);
+Console.WriteLine($"ennyi az indiai nagyvárosok átlaglélekszáma: {f2}");
+
+var f3 = adatoks.MaxBy(v => v.Nepesseg);
+Console.WriteLine($"ez a nagyvárosa a legnépesebb: {f3.VarosNeve}");
+
+var f4 = adatoks.Where(v => v.Nepesseg > 20000000).OrderByDescending(v => v.Nepesseg).ToList();
+
+
+
+foreach (var item in f4)
+{
+    Console.WriteLine($"{item.VarosNeve} - {item.Nepesseg}");
+}
+
+
+var f5 = adatoks.GroupBy(v => v.OrszagNeve).Where(v => v.Count() > 1).Count();
+Console.WriteLine($"Azon oszágok száma hol több nagyváros is szerepel: {f5}");
+
+
+var f6 = adatoks.GroupBy(v => v.VarosNeve[0]).OrderByDescending(g => g.Count()).First();
+
+Console.WriteLine($"A legtöbb nagyváros neve {f6.Key}-vel kezdődik, összesen {f6.Count()} város.");
+
+
+
